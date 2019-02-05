f8f07fd3e1f6c0a0492e3e995c867cde0eb822df
Sydney <stolbert3@gatech.edu>
2019-02-05 16:04:56 -0500

    removed unnecessary files

--- routes/api/article.js
+++
@@ -1,6 +0,0 @@
-var router = require("express").Router();
-var articleController = require("../../controllers/article");
-
-router.get("/", articleController.findAll);
-
-module.exports = router;
--- routes/api/comment.js
+++
@@ -1,7 +0,0 @@
-var router = require("express").Router();
-var noteController = require("../../controllers/comment");
-
-router.get("/:id", noteController.find);
-router.post("/", noteController.create);
-
-module.exports = router;
